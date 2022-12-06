ecd7feba84e946409862e00a661a2b8a46279cfe
vganapati <vganapa1@swarthmore.edu>
2022-12-05 16:59:49 -0800

    day 3 solved

--- 2022/03/aoc202203.py
+++ 2022/03/aoc202203.py
@@ -3,18 +3,53 @@
 # Standard library imports
 import pathlib
 import sys
-
+import numpy as np
 
 def parse_data(puzzle_input):
     """Parse input."""
+    all_rucksacks = puzzle_input.split("\n")
+    return [[item for item in items_per_rucksack] for items_per_rucksack in all_rucksacks]
 
+def ord_to_priority(letter):
+    '''
+    ord('A') == 65
+    ord('Z') == 90
+    
+    ord('a') == 97
+    ord('z') == 122
+    '''
+    if ord(letter)<91: # capital letter
+        return(ord(letter)-38)
+    else: # lowercase letter
+        return(ord(letter)-96)
+      
+    
 
 def part1(data):
     """Solve part 1."""
+    
+    # split each rucksack into 2 compartments
+    data = [np.array_split(rucksack,2) for rucksack in data]
 
-
+    # find the item that is the same in both rucksack
+    data = [np.intersect1d(compartments[0],compartments[1]) for compartments in data]
+    
+    '''
+    Find the priorities for each of the common items:
+    Lowercase item types a through z have priorities 1 through 26.
+    Uppercase item types A through Z have priorities 27 through 52.
+    '''
+    return np.sum([ord_to_priority(i[0][0]) for i in data])
+    
+    
 def part2(data):
     """Solve part 2."""
+    
+    # indices corresponding to groups of 3
+    inds = np.arange(0,len(data),3)
+    data = [np.intersect1d(np.intersect1d(data[ind],data[ind+1]),data[ind+2]) for ind in inds]
+    
+    return np.sum([ord_to_priority(i[0][0]) for i in data])
 
 
 def solve(puzzle_input):
--- 2022/03/test_aoc202203.py
+++ 2022/03/test_aoc202203.py
@@ -22,25 +22,43 @@ def example2():
     return aoc202203.parse_data(puzzle_input)
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
-    assert example1 == ...
+    assert example1 == [['v','J','r','w','p','W','t','w','J','g','W','r','h','c','s','F','M','M','f','F','F','h','F','p'],
+                        ['j','q','H','R','N','q','R','j','q','z','j','G','D','L','G','L','r','s','F','M','f','F','Z','S','r','L','r','F','Z','s','S','L'],
+                        ['P','m','m','d','z','q','P','r','V','v','P','w','w','T','W','B','w','g'],
+                        ['w','M','q','v','L','M','Z','H','h','H','M','v','w','L','H','j','b','v','c','j','n','n','S','B','n','v','T','Q','F','n'],
+                        ['t','t','g','J','t','R','G','J','Q','c','t','T','Z','t','Z','T'],
+                        ['C','r','Z','s','J','s','P','P','Z','s','G','z','w','w','s','L','w','L','m','p','w','M','D','w']]
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc202203.part1(example1) == ...
+    assert aoc202203.part1(example1) == 157
 
 
+def test_ord_to_priority1():
+    assert aoc202203.ord_to_priority('a') == 1
+
+
+def test_ord_to_priority2():
+    assert aoc202203.ord_to_priority('A') == 27
+
+  
+def test_ord_to_priority3():
+    assert aoc202203.ord_to_priority('z') == 26
+
+  
+def test_ord_to_priority4():
+    assert aoc202203.ord_to_priority('Z') == 52
+
+  
 @pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1):
     """Test part 2 on example input."""
     assert aoc202203.part2(example1) == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_example2(example2):
     """Test part 2 on example input."""
-    assert aoc202203.part2(example2) == ...
+    assert aoc202203.part2(example2) == 70
