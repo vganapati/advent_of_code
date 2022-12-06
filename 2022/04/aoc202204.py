844b38d555391b91208d53e968b23626f0b08c8e
vganapati <vganapa1@swarthmore.edu>
2022-12-06 09:41:14 -0800

    day 4 solution

--- 2022/04/aoc202204.py
+++ 2022/04/aoc202204.py
@@ -4,17 +4,45 @@
 import pathlib
 import sys
 
-
 def parse_data(puzzle_input):
     """Parse input."""
-
+    all_pairs = puzzle_input.split("\n")
+    return [[[int(item) for item in elf.split("-")] for elf in pair.split(",")] for pair in all_pairs]
 
 def part1(data):
     """Solve part 1."""
+    count=0
+    for pair in data:
+        elf_0 = pair[0]
+        elf_1 = pair[1]
 
+        if (elf_0[0]==elf_1[0]) or (elf_0[1]==elf_1[1]):
+            # print('same start or end index')
+            count+=1
+        elif (elf_0[0]<elf_1[0]):
+            if elf_1[1]<elf_0[1]:
+                # print('second is enclosed in first')
+                count+=1
+        else:
+          if elf_0[1]<elf_1[1]:
+              # print('first is enclosed in second')
+            count+=1
+            
+    return count
+        
+    
 
 def part2(data):
     """Solve part 2."""
+    total_pairs = len(data)
+    count=0 # no overlap at all
+    for pair in data:
+        elf_0 = pair[0]
+        elf_1 = pair[1]
+        if (elf_0[0]>elf_1[1]) or (elf_1[0]>elf_0[1]):
+            count+=1
+    return(total_pairs-count)
+            
 
 
 def solve(puzzle_input):
--- 2022/04/test_aoc202204.py
+++ 2022/04/test_aoc202204.py
@@ -22,25 +22,41 @@ def example2():
     return aoc202204.parse_data(puzzle_input)
 
 
-@pytest.mark.skip(reason="Not implemented")
+@pytest.fixture
+def example3():
+    puzzle_input = (PUZZLE_DIR / "example3.txt").read_text().strip()
+    return aoc202204.parse_data(puzzle_input)
+  
+
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
-    assert example1 == ...
+    assert example1 == [[[2,4],[6,8]],
+                        [[2,3],[4,5]],
+                        [[5,7],[7,9]],
+                        [[2,8],[3,7]],
+                        [[6,6],[4,6]],
+                        [[2,6],[4,8]]]
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc202204.part1(example1) == ...
+    assert aoc202204.part1(example1) == 2
 
 
-@pytest.mark.skip(reason="Not implemented")
+def test_part1_example2(example2):
+    """Test part 1 on example input."""
+    assert aoc202204.part1(example2) == 8
+    
+def test_part1_example3(example3):
+    """Test part 1 on example input."""
+    assert aoc202204.part1(example3) == 1
+
+
 def test_part2_example1(example1):
     """Test part 2 on example input."""
-    assert aoc202204.part2(example1) == ...
+    assert aoc202204.part2(example1) == 4
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_example2(example2):
     """Test part 2 on example input."""
-    assert aoc202204.part2(example2) == ...
+    assert aoc202204.part2(example2) == 13
