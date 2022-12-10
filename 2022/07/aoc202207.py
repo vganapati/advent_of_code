beb69138c858675519a85b20feeb01351794f3f3
vganapati <vganapa1@swarthmore.edu>
2022-12-10 13:24:42 -0800

    day 7 attempt

---
+++ 2022/07/aoc202207.py
@@ -0,0 +1,92 @@
+"""AoC 7, 2022."""
+
+# Standard library imports
+import pathlib
+import sys
+from anytree import AnyNode,PreOrderIter
+import numpy as np
+
+def parse_data(puzzle_input):
+    """Parse input."""
+    data = puzzle_input.split("\n")
+    return(data)
+
+def total_size(data):
+    file_total=0
+    for dat in data:
+        if (dat[0] != '$') and (dat[:3] != 'dir'):
+            size,node_id = dat.split(" ")
+            file_total += int(size)
+    return(file_total)
+
+def create_tree(data):
+    nodes = []
+    
+    parent = AnyNode(id='root')
+    nodes.append(parent)
+    
+    count = 0
+    while count<len(data):
+        if data[count] == '$ ls':
+            # keep reading lines until a '$'
+            count+=1
+            while count<len(data) and data[count][0] != '$' :
+                if data[count][:3] == 'dir':
+                    pass
+                else:
+                    size,node_id = data[count].split(" ")
+                    node = AnyNode(id=node_id,parent=parent,size=int(size),folder=False)
+                    nodes.append(node)
+                count+=1
+        elif data[count][:7] == '$ cd ..':
+            parent = parent.parent
+            count+=1
+        elif data[count][:4] == '$ cd':
+            node_id = data[count][5:]
+            node = AnyNode(id=node_id, parent=parent, folder=True, size=0)
+            nodes.append(node)
+            parent=node
+            count+=1  
+    return(nodes)
+
+def get_size(node):
+    size = np.sum([node.size for node in node.children])
+    for node in node.children:
+        if node.folder == True:
+            size+=get_size(node)
+    return(size)
+        
+
+def part1(data):
+    """Solve part 1."""
+    nodes=create_tree(data)
+    size_dict={}
+    for node in PreOrderIter(nodes[1]):
+        if node.folder == True:
+            size_dict[node.id]=get_size(node)
+            
+    total=0
+    for key in size_dict.keys():
+        if size_dict[key]<=100000:
+            total+=size_dict[key]
+    return(total)
+            
+
+    
+
+def part2(data):
+    """Solve part 2."""
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse_data(puzzle_input)
+    yield part1(data)
+    yield part2(data)
+
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"\n{path}:")
+        solutions = solve(puzzle_input=pathlib.Path(path).read_text().rstrip())
+        print("\n".join(str(solution) for solution in solutions))
---
+++ 2022/07/test_aoc202207.py
@@ -0,0 +1,110 @@
+"""Tests for AoC 7, 2022."""
+
+# Standard library imports
+import pathlib
+
+# Third party imports
+import aoc202207
+import pytest
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+
+@pytest.fixture
+def example1():
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().rstrip()
+    return aoc202207.parse_data(puzzle_input)
+
+
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().rstrip()
+    return aoc202207.parse_data(puzzle_input)
+
+
+@pytest.fixture
+def example3():
+    puzzle_input = (PUZZLE_DIR / "example3.txt").read_text().rstrip()
+    return aoc202207.parse_data(puzzle_input)
+
+def test_parse_example1(example1):
+    """Test that input is parsed properly."""
+    assert example1 == ['$ cd /',
+                        '$ ls',
+                        'dir a',
+                        '14848514 b.txt',
+                        '8504156 c.dat',
+                        'dir d',
+                        '$ cd a',
+                        '$ ls',
+                        'dir e',
+                        '29116 f',
+                        '2557 g',
+                        '62596 h.lst',
+                        '$ cd e',
+                        '$ ls',
+                        '584 i',
+                        '$ cd ..',
+                        '$ cd ..',
+                        '$ cd d',
+                        '$ ls',
+                        '4060174 j',
+                        '8033020 d.log',
+                        '5626152 d.ext',
+                        '7214296 k']
+
+
+
+def test_parse_example2(example2):
+    """Test that input is parsed properly."""
+    assert example2 == ['$ cd /',
+                        '$ ls',
+                        'dir ccjp',
+                        '328708 hglnvs.bsh',
+                        'dir hpsnpc',
+                        'dir pcb',
+                        'dir pntzm',
+                        'dir pzg',
+                        'dir thfgwwsp',
+                        '$ cd ccjp',
+                        '$ ls',
+                        '159990 dlz',
+                        'dir mbtsvblj',
+                        '165076 nppbjl.qhg',
+                        '$ cd mbtsvblj',
+                        '$ ls',
+                        '34806 frqsf.nsv',
+                        'dir ppq',
+                        'dir ptht',
+                        'dir rgmvdwt',
+                        '$ cd ppq',
+                        '$ ls',
+                        '10 dhzp',
+                        '$ cd ..',
+                        '$ cd ptht']
+
+def test_part1_example1(example1):
+    """Test part 1 on example input."""
+    assert aoc202207.part1(example1) == 95437
+
+def test_part1_example2(example2):
+    """Test part 1 on example input."""
+    assert aoc202207.part1(example2) == 34826
+
+def test_total_size(example1):
+    assert aoc202207.total_size(example1) == 48381165
+
+def test_part1_example3(example3):
+    """Test part 1 on example input."""
+    assert aoc202207.part1(example3) == 81645
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example1(example1):
+    """Test part 2 on example input."""
+    assert aoc202207.part2(example1) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example2(example2):
+    """Test part 2 on example input."""
+    assert aoc202207.part2(example2) == ...
